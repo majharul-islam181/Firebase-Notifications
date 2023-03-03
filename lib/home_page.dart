b2d9d1b4ca4c99f1999bea2dc50e2008a4d4d9fd
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2023-03-03 16:29:53 +0600

    firebaseNotifications

---
+++ lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'notification_services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final NotificationServices _notificationServices = NotificationServices();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _notificationServices.requestNotificationsPermition();
+    super.initState();
+
+
+      _notificationServices.getDeviceToken().then((value) => 
+    
+      print(value),
+      
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: const  Text("This is title"),),
+    );
+  }
+}
---
+++ lib/notification_services.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationServices {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  void requestNotificationsPermition() async {
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: true,
+      badge: true,
+      carPlay: true,
+      criticalAlert: true,
+      provisional: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('user granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('user  grandted provisional');
+    } else {
+      print("denied the permission");
+    }
+  }
+
+    Future<String?> getDeviceToken() async {
+      String? token = await messaging.getToken();
+      return token;
+    }
+}
